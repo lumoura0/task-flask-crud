abcbb29bcf2ccfc2251f36d1d64d016c65331e84
Luis Fernando <luisfernando.mourag@gmail.com>
2025-08-06 20:19:04 -0300

    Desenvolvimento do CRUD com Flask - Read - parte 1

--- app.py
+++ app.py
@@ -19,5 +19,16 @@ def create_task():
     print(f"Task created: {new_tast}")
     return jsonify({"message":"Nova tarefa criada com sucesso!"})
 
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    # Logic to get all tasks
+    task_list = [task.to_dict() for task in tasks] # Convert Task objects to dictionaries
+    output = {
+                "tasks": task_list,
+                "total_tasks": 0
+            }
+    return jsonify(output)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
