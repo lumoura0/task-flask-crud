02d7835cf2abaa4188b10cc820b93c5fc92cae42
Luis Fernando <luisfernando.mourag@gmail.com>
2025-08-06 19:33:01 -0300

    Desenvolvimento do CRUD com Flask Create - parte 2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from models.task import Task
 
 app = Flask(__name__)
@@ -6,13 +6,18 @@ app = Flask(__name__)
 # CRUD operations
 # Create, Read, Update, Delete
 tasks = []
-
+task_id_control = 1
 @app.route('/tasks', methods=['POST'])
 def create_task():
     # Logic to create a new task
+    global task_id_control
     data = request.get_json()
-    print(data)
-    return 'Test'
+    new_tast = Task(id=task_id_control, title=data["title"], 
+    description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_tast)
+    print(f"Task created: {new_tast}")
+    return jsonify({"message":"Nova tarefa criada com sucesso!"})
 
 if __name__ == "__main__":
     app.run(debug=True)
